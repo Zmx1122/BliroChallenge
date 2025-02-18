e4408cdb02dd210f735d6d954211e8c3e40dfdf4
Zmx1122 <110481934+Zmx1122@users.noreply.github.com>
2025-02-18 22:23:21 +0100

    Update evaluation_criteria.py

--- evaluation_criteria.py
+++ evaluation_criteria.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 18 15:21:50 2025
-
-@author: zhang
-"""
-
 import nltk
 from nltk import word_tokenize, pos_tag, ne_chunk
 from langdetect import detect
