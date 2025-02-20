d3ca6a2fa9fca23ecdace43e49bd45e43cea09ab
Zmx1122 <110481934+Zmx1122@users.noreply.github.com>
2025-02-20 15:22:21 +0100

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-# Store OpenAI API Key
-
-API_KEY = "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"  
